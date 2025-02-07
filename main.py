2689ba32fa5cd9586ef980fa790400d70f389780
LucasMarquesCorrea <lucas.correa@aluno.ufabc.aluno.edu.br>
2025-02-06 21:05:02 -0300

    :lipstick: feat: criando aplicacao streamlit

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+
+    st.title("streamlitDataQuality")
+    st.write("This is a demo of the streamlitDataQuality package.")
+
+
+if __name__ == "__main__":
+    main()
